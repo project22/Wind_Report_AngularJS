960f109c2f381f6d76f708176e95082d8f2812d4
JP Berti <jpberti@yahoo.com>
2014-01-30 13:52:27 -0800

    add google place lookup

--- js/windfinder.js
+++ js/windfinder.js
@@ -1,21 +1,67 @@
+var city="Santa Monica";
+
 var App = angular.module('App', []);
 
 App.controller('WindReportCtrl', function($scope, $http) {
 
-	// url = "/mock_api/San_Francisco.json"
-	url = "http://api.wunderground.com/api/3edda987ad0a169a/hourly10day/q/CA/Santa_Monica.json?callback=JSON_CALLBACK";
-
-  $http.jsonp(url)
-    .success(function(data){
-      $scope.weather = data.hourly_forecast;    
-      console.log(data.hourly_forecast);   
-                    
-  })
+	$scope.getWindData = function() {
+		// alert("fired");
+		url = "http://api.wunderground.com/api/3edda987ad0a169a/hourly10day/q/"+ city + ".json?callback=JSON_CALLBACK";
+	  $http.jsonp(url)
+	    .success(function(data){
+	      $scope.weather = data.hourly_forecast;                     
+	  })
+	}
 });
 
 
-function FirstCtrl($scope){
-  $scope.data = {message: "Hello"};
+// function FirstCtrl($scope){
+//   $scope.data = {message: "Hello"};
+// }
+
+
+$(function() {
+  initialize();
+  foundation()
+});
+
+
+// Google Autocomplete
+
+var autocomplete;
+
+function initialize() {
+  // Create the autocomplete object, restricting the search
+  // to geographical location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {HTMLInputElement} */(document.getElementById('city')),
+      { types: ['geocode'] });
+  // When the user selects an address from the dropdown,
+  // populate the address fields in the form.
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    
+    var place = autocomplete.getPlace();
+	  console.log(place);
+	  // 37.8,-122.4
+	  city = place.geometry.location.d + "," + place.geometry.location.e;
+	  // city = place.address_components[0].long_name + ", " + place.address_components[2].long_name + ", " + place.address_components[3].long_name;
+	  console.log(city);
+    
+  });
 }
 
-// 3edda987ad0a169a
+// [START region_geolocation]
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = new google.maps.LatLng(
+          position.coords.latitude, position.coords.longitude);
+      autocomplete.setBounds(new google.maps.LatLngBounds(geolocation,
+          geolocation));
+    });
+  }
+}
+// [END region_geolocation]
+
